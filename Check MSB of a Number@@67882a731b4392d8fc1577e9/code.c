114aa44e0416b4306168e9fec713d426f7982c4f
Mohit <mohit3333singla@gmail.com>
2025-04-07 17:46:41 +0530

    Solution to Check MSB of a Number

--- Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
 int main() {
     int a;
     scanf("%d",&a);
-    if(a !& 1){
+    int msb_mask=1<<31;
+    if(a & msb_mask){
         printf("Set");
     }
     else{
         printf("Not Set");
     }
     return 0;
-}
+}   
